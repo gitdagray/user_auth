e25265c7b4be78fbb46e2ea70a3eae35727a1170
gitdagray <DaveOnEleven@gmail.com>
2021-09-30 15:42:16 -0500

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,22 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+const bcrypt = require('bcrypt');
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+    const foundUser = usersDB.users.find(person => person.username === user);
+    if (!foundUser) return res.sendStatus(401); //Unauthorized 
+    // evaluate password 
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+        // create JWTs
+        res.json({ 'success': `User ${user} is logged in!` });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { handleLogin };
